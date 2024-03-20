3e9eb3ecf79f2628a385deddd6cf8704db4e348a
keaunsol Na <knsol1992@naver.com>
2024-03-20 10:18:52 +0900

    사칙연산 계산기 servlet 추가

---
+++ WhiteProgrameDesignStudy/src/keaunsol/calculatorModule/CalculatorServlet.java
@@ -0,0 +1,55 @@
+package org.example;
+
+import org.example.calculator.domain.Calculator;
+import org.example.calculator.domain.PositiveNumber;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import javax.servlet.*;
+import javax.servlet.annotation.WebServlet;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet("/servlet/calculate")
+public class CalculatorServlet implements Servlet {
+    private static final Logger log = LoggerFactory.getLogger(CalculatorServlet.class);
+    private ServletConfig servletConfig;
+
+
+    @Override
+    public void init(ServletConfig servletConfig) throws ServletException {
+        log.info("init");
+        this.servletConfig = servletConfig;
+
+    }
+
+    @Override
+    public void service(ServletRequest request, ServletResponse response) throws ServletException, IOException {
+        log.info("service");
+        int operand1 = Integer.parseInt(request.getParameter("operand1"));
+        String operator = request.getParameter("operator");
+        int operand2 = Integer.parseInt(request.getParameter("operand2"));
+
+        int result = Calculator.calculate(new PositiveNumber(operand1), operator, new PositiveNumber(operand2));
+
+        PrintWriter writer = response.getWriter();
+        writer.println(request);
+    }
+
+    @Override
+    public void destroy() {
+
+        // resource release
+    }
+
+    @Override
+    public ServletConfig getServletConfig() {
+        return this.servletConfig;
+    }
+
+    @Override
+    public String getServletInfo() {
+        return null;
+    }
+
+}
---
+++ WhiteProgrameDesignStudy/src/keaunsol/calculatorModule/CalculatorServlet2.java
@@ -0,0 +1,32 @@
+package org.example;
+
+import org.example.calculator.domain.Calculator;
+import org.example.calculator.domain.PositiveNumber;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import javax.servlet.*;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet("/servlet/calculate2")
+public class CalculatorServlet2 extends HttpServlet {
+    private static final Logger log = LoggerFactory.getLogger(CalculatorServlet2.class);
+    @Override
+    public void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+        log.info("service");
+        int operand1 = Integer.parseInt(request.getParameter("operand1"));
+        String operator = request.getParameter("operator");
+        int operand2 = Integer.parseInt(request.getParameter("operand2"));
+
+        int result = Calculator.calculate(new PositiveNumber(operand1), operator, new PositiveNumber(operand2));
+
+        PrintWriter writer = response.getWriter();
+        writer.println(request);
+    }
+}
